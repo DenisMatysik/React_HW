5ca5c9259975f8904c8fea6310bfb9fbd15bb67f
denis <denismatysik@gmail.com>
2022-02-17 17:46:17 +0300

    diplom

--- src/diplom/components/todoItem/TodoItem.jsx
+++ src/diplom/components/todoItem/TodoItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { MdDelete, MdEdit } from 'react-icons/md'
+import { useDispatch } from 'react-redux'
 import "./TodoItem.css"
-
-
+import { deleteTodo } from '../redux/todoSlice'
+import toast from 'react-hot-toast'
 
 export default function TodoItem({todo}) {
-const deleteTodo =()=>{
-    console.log("deleteTodo")
+const dispatch = useDispatch();
+const deleteTask =()=>{
+    dispatch(deleteTodo(todo.id));
+    toast.success("Todo was deleted")
 }
 const editTodo =()=>{
     console.log("editTodo")
@@ -21,18 +24,18 @@ const editTodo =()=>{
             </div>
         </div>
         <div className='todoActions'>
-        <div className='icon'
-        onClick={deleteTodo}
-        role="button"
-        tabIndex={0}>
-            <MdEdit/>
+            <div className='icon'
+            onClick={editTodo}
+            role="button"
+            tabIndex={0}>
+                <MdEdit/>
+                </div>
+            <div className='icon'
+            onClick={deleteTask}
+            role="button"
+            tabIndex={0}>
+                <MdDelete/>
             </div>
-        <div className='icon'
-        onClick={editTodo}
-        role="button"
-        tabIndex={0}>
-            <MdDelete/>
-        </div>
         </div>
     </div>
   )

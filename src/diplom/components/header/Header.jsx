246446b242c49db6720e530e4eac06a922e22a05
denis <denismatysik@gmail.com>
2022-02-19 22:23:55 +0300

    diplom

--- src/diplom/components/header/Header.jsx
+++ src/diplom/components/header/Header.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
-import Button, { SelectButton } from '../button/Button'
+import { useDispatch } from 'react-redux'
+import Button from '../button/Button'
 import '../button/Button.css'
 import Modal from '../modal/Modal'
+import { uptadeFilterStatus } from '../redux/todoSlice'
 import "./Header.css"
 
 export default function Header() {
-    const [modalOpen, setModalOpen] = useState(false)
+    const [modalOpen, setModalOpen] = useState(false);
+    const dispatch = useDispatch();
+
+    const updateFilter = (e)=>{
+      console.log(e.target.value)
+      dispatch(uptadeFilterStatus(e.target.value))
+    }
   return (
     <div className='headerNav'>
         <div className='appHeader'>
-        <Button onClick={()=>setModalOpen(true)} typeButton="submit" variant='button--primary'>Add tusk</Button>
-        <Button typeButton="button" variant='button--all'>All task</Button>
-        <Button typeButton="button" variant='button--all'>Complelte task</Button>
-        <Button typeButton="button" variant='button--all'>Incomplelte task</Button>
+        <Button onClick={()=>setModalOpen(true)} typeButton="submit" variant='button--primary'>Add task</Button>
+        <Button typeButton="button" variant='button--all' value="all" onClick={updateFilter}>All task</Button>
+        <Button typeButton="button" variant='button--all' value="complete" onClick={updateFilter}>Complelte task</Button>
+        <Button typeButton="button" variant='button--all' value="incomplete" onClick={updateFilter}>Incomplelte task</Button>
+
         </div>
         <Modal type="add" modalOpen={modalOpen} setModalOpen={setModalOpen}/>
     </div>
